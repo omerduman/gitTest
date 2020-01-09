8172a3286b2fe0f0f38237ee193a312832c16e71
omerduman <dumanomer200@gmail.com>
2020-01-09 18:40:07 +0300

    Initial Commit

---
+++ gitdeneme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitdeneme
+//
+//  Created by admin on 9.01.2020.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
